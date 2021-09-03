67f35cfbf2aa04c434eafb197d0bdd4b840492b4
semyaat <semya@erlito.com>
2021-09-03 12:54:22 +0200

    added iomanip to prob2

--- Problem2.cpp
+++ Problem2.cpp
@@ -4,11 +4,7 @@
 #include <fstream> // acces to ofstream class 
 #include <math.h>
 #include <armadillo>
-<<<<<<< HEAD
 #include <iomanip>
-=======
-#include <iomanip> 
->>>>>>> 4491fc5f703cd812698cc1e3a57bf10ca210d876
 
 using namespace std;
 using namespace arma;
