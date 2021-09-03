5f67f942385b7d373ccc5bbeed43c624facdbc37
semyaat <semya@erlito.com>
2021-09-03 11:36:49 +0200

    added Problem7

---
+++ Problem7.cpp
@@ -0,0 +1,9 @@
+//Prob. 7
+
+#include <iostream> // input and output 
+#include <fstream> // access to ofstream class 
+#include <math.h>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
