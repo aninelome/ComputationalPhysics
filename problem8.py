1c1f842ffa81a0bb4e9642f3d7b47d9522d09860
elisemma <elise.malmer.martinsen@outlook.com>
2021-09-07 16:12:28 +0200

    Redigert prob8

--- problem8.py
+++ problem8.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def hentdata(filename1, filename2):
+def hentdata(filename1, filename2): #Henter ut x, u og v fra filer
     x = []
     V_x = []
     U_x = []
@@ -26,23 +26,49 @@ def hentdata(filename1, filename2):
     X = np.array(x)
     U = np.array(U_x)
     V = np.array(V_x)
+
+    return X, U, V
+
+
+def Delta_i(U,V): #returnerer absolutt feil
     delta = np.log10(np.abs(U-V))
-    epsiloni = np.log10(np.abs((U-V)/U))
+    return delta
 
+def epsilon_i(U,V): #returnerer relativ feil
+    epsilon = np.log10(np.abs((U-V)/U))
+    return epsilon
+
+#Lager lister med filnavnene vi skal hente ut data fra
+filename1 = ["prob7_n_10.txt", "prob7_n_100.txt", "prob7_n_1000.txt"]
+filename2 = ["prob2_n_10.txt", "prob2_n_100.txt", "prob2_n_1000.txt"]
+
+#Regner ut og plotter absolutt feil for ulike n
+for i in range 3:
+    x, u, v = hentdata(filename1[i], filename2[i])
     plt.plot(X, delta, label = f"n = {n}")
-    plt.plot(X, epsiloni, label = f"n = {n}")
-    return
 
+#Lager mappe til bildene
 path = "./results/"
 if not os.path.exists(path):
     os.makedirs(path)
 
+#Fullfører plott med absolutt error
 plt.legend()
 plt.title("Absolute error for different n")
 plt.xlabel("x")
 plt.ylabel("$\Delta_i$")
 plt.savefig("delta_i.pdf")
 plt.show()
-hentdata("prob7_n_10.txt", "prob2_n_10.txt")
-hentdata("prob7_n_100.txt", "prob2_n_100.txt")
-hentdata("prob7_n_1000.txt", "prob2_n_1000.txt")
+
+#Regner ut og plotter absolutt feil for ulike n
+for i in range 3:
+    x, u, v = hentdata(filename1[i], filename2[i])
+    plt.plot(X, epsilon, label = f"n = {n}")
+
+#Fullfører plott med relativ feil
+plt.legend()
+plt.title("Relative error for different n")
+plt.xlabel("x")
+plt.ylabel("$\epsilon_i$")
+plt.savefig("epsilon_i.pdf")
+plt.show()
