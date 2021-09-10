b6527f8a4a5b34b71e762ef02d1f4f73f3b6090b
semyaat <semya@erlito.com>
2021-09-10 15:45:42 +0200

    9 ok, working on 10

--- Problem10.cpp
+++ Problem10.cpp
@@ -13,83 +13,79 @@ using namespace arma;
 
 
 void V(int n, string filename){
-      double h = 1./(n-1);
-      double hh = h*h;
-      vec a(n), b(n), c(n), g(n), v(n);
+    vec a(n), b(n), c(n), g(n), v(n); // define vectors 
 
+    // fill vectors a, b, c with values
+    a.fill( -1);
+    b.fill( 2);
+    c.fill( -1);
 
-      // fill vectors a, b, c with values
-      a.fill( -1);
-      b.fill( 2);
-      c.fill( -1);
+    vec x = linspace(0, 1, n); // define x from 0 to 1 
+    double h = x(1)-x(0);      // define h and h^2 
+    double hh = h*h;
+    vec f_x = 100.0*exp(-10*x);
 
-      vec x = linspace(0, 1, n);
-      vec f_x = 100.0*exp(-10*x);
+    // set initial values for g 
+    g(0) = hh*f_x(0);
+    g = hh*f_x;
 
-      g(0) = hh*f_x(0);
-      g = hh*f_x;
+    // Begin Algorithm 
+    for (int i = 2; i <= n-2; i++) {
+        b(i) = b(i) - a(i)/b(i-1) * c(i-1);
+        g(i) = g(i) - a(i)/b(i-1) * g(i-1);
+    }
 
-
-      for (int i = 1; i <= n-2; i++) {
-          b(i) = b(i) - a(i)/b(i-1) * c(i-1);
-          g(i) = g(i) - a(i)/b(i-1) * g(i-1);    
-      }
-
-
-    v(0) = 0;
-    v(n-1) = 0;
+    // define initial values for v 
+    v(0) = v(n-1) = 0;
     v(n-2) = g(n-2)/b(n-2);
 
-    for (int i = n-3; i >= 1; i--) {
+    for (int i = n-3; i >= 1; i--) {              // calculate v 
         v(i) = (g(i) - c(i)*v(i+1)) / b(i);
-
     }
-    
-    // write x and U(x) to a textfile
+
+    // write x and v to a textfile
     ofstream file;
     file.open(filename, ios::out); //opens file in out/write mode
-
     for (int i = 0; i < n; i++){
         file << setw(25) << setprecision(3) << x(i);
         file <<fixed<< setw(25) << setprecision(8) << v(i) << endl;
-
     }
-
     file.close();
-    
+
     return;
 }
 
-
 void V_special(int n, string filename){
-    double h = 1./(n-1);
-    double hh = h*h;
-    vec a(n), b(n), c(n), g(n), v(n);
+    vec a(n), b(n), c(n), g(n), v(n); // define vectors 
 
     // fill vectors a, b, c with values
     a.fill(-1);
     b.fill(2);
     c.fill(-1);
 
-    vec x = linspace(0, 1, n);
+    vec x = linspace(0, 1, n);         // define x from 0 to 1 
+    double h = x(1) - x(0);            // define h and h^2 
+    double hh = h*h;
     vec f_x = 100.0*exp(-10*x);
 
+    // set initial values for g 
     g(0) = hh*f_x(0);
     g = hh*f_x;
     
-    // calculate b outside the algorithm 
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) {                 // calculate b outside the main algorithm 
         b(i) = (i + 1.)/i;
     }
     
-    for (int i = 1; i <= n-2; i++) {
+    // Begin Algorithm 
+    for (int i = 2; i <= n-2; i++) {              // calculate g 
         g(i) = g(i) - a(i)/b(i-1) * g(i-1);    
     }
 
+    // Define initial values for v 
     v(0) = v(n-1) = 0;
     v(n-2) = g(n-2)/b(n-2);
 
-    for (int i = n-3; i > 0; i--) {
+    for (int i = n-3; i >= 1; i--) {                // calculate v
         v(i) = (g(i) - c(i)*v(i+1)) / b(i);
     }
 
@@ -105,7 +101,6 @@ void V_special(int n, string filename){
     file.close();
     return;
 }
-
 
 int main () {
     //int n = pow(10,6); 
--- Problem9.cpp
+++ Problem9.cpp
@@ -12,34 +12,35 @@ using namespace std;
 using namespace arma;
 
 void V_special(int n, string filename){
-    double h = 1./(n-1);
-    double hh = h*h;
-    vec a(n), b(n), c(n), g(n), v(n);
+    vec a(n), b(n), c(n), g(n), v(n); // define vectors 
 
     // fill vectors a, b, c with values
     a.fill(-1);
     b.fill(2);
     c.fill(-1);
 
-    vec x = linspace(0, 1, n);
+    vec x = linspace(0, 1, n);         // define x from 0 to 1 
+    double h = x(1) - x(0);            // define h and h^2 
+    double hh = h*h;
     vec f_x = 100.0*exp(-10*x);
 
+    // set initial values for g 
     g(0) = hh*f_x(0);
     g = hh*f_x;
     
-    // calculate b outside the algorithm 
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) {                 // calculate b outside the algorithm 
         b(i) = (i + 1.)/i;
     }
     
-    for (int i = 1; i <= n-2; i++) {
+    for (int i = 2; i <= n-2; i++) {              // calculate g 
         g(i) = g(i) - a(i)/b(i-1) * g(i-1);    
     }
 
+    // Define initial values for v 
     v(0) = v(n-1) = 0;
     v(n-2) = g(n-2)/b(n-2);
 
-    for (int i = n-3; i > 0; i--) {
+    for (int i = n-3; i >= 1; i--) {                // calculate v
         v(i) = (g(i) - c(i)*v(i+1)) / b(i);
     }
 
@@ -57,8 +58,10 @@ void V_special(int n, string filename){
 }
 
 int main(int argc, char const *argv[]){ 
+    // run V_special for n=10
     for (int i = 1; i <= 1; i++) {
         int n = pow(10, i);
+        cout << "n = " << n << endl;
         V_special(n, "prob9_n_" + to_string(n) + ".txt");
     }
     return 0;
