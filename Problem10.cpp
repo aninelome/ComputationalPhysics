b6d61a1026060e23ca7ab925e9cf6decbdc251e4
semyaat <semya@erlito.com>
2021-09-10 12:46:14 +0200

    Prob 9 and 10 ok

--- Problem10.cpp
+++ Problem10.cpp
@@ -44,7 +44,7 @@ void V(int n, string filename){
         v(i) = (g(i) - c(i)*v(i+1)) / b(i);
 
     }
-
+    
     // write x and U(x) to a textfile
     ofstream file;
     file.open(filename, ios::out); //opens file in out/write mode
@@ -56,49 +56,50 @@ void V(int n, string filename){
     }
 
     file.close();
+    
     return;
 }
 
+
 void V_special(int n, string filename){
-      double h = 1./(n-1);
-      double hh = h*h;
-      vec g(n), v(n);
+    double h = 1./(n-1);
+    double hh = h*h;
+    vec a(n), b(n), c(n), g(n), v(n);
 
-    // all vectors are same number 
-      int a, c = -1;
-      int b = 3/2;
+    // fill vectors a, b, c with values
+    a.fill(-1);
+    b.fill(2);
+    c.fill(-1);
 
-      vec x = linspace(0, 1, n);
-      vec f_x = 100.0*exp(-10*x);
+    vec x = linspace(0, 1, n);
+    vec f_x = 100.0*exp(-10*x);
 
-      g(0) = hh*f_x(0);
-      g = hh*f_x;
-
-      //b = 3/2; 
-      float CONST = -1/2; 
-
-      for (int i = 1; i <= n-2; i++) {
-          g(i) = g(i) - CONST * g(i-1);    
-      }
-
-
-    v(0) = 0;
-    v(n-1) = 0;
-    v(n-2) = g(n-2)/b;
-
-    for (int i = n-3; i >= 1; i--) {
-        v(i) = g(i) / b - CONST*v(i+1);
-
+    g(0) = hh*f_x(0);
+    g = hh*f_x;
+    
+    // calculate b outside the algorithm 
+    for (int i = 1; i < n; i++) {
+        b(i) = (i + 1.)/i;
+    }
+    
+    for (int i = 1; i <= n-2; i++) {
+        g(i) = g(i) - a(i)/b(i-1) * g(i-1);    
     }
 
-    // write x and U(x) to a textfile
+    v(0) = v(n-1) = 0;
+    v(n-2) = g(n-2)/b(n-2);
+
+    for (int i = n-3; i > 0; i--) {
+        v(i) = (g(i) - c(i)*v(i+1)) / b(i);
+    }
+
+    // write x and v to a textfile
     ofstream file;
     file.open(filename, ios::out); //opens file in out/write mode
 
     for (int i = 0; i < n; i++){
         file << setw(25) << setprecision(3) << x(i);
         file <<fixed<< setw(25) << setprecision(8) << v(i) << endl;
-
     }
 
     file.close();
@@ -112,7 +113,7 @@ int main () {
     // Begin measuring time for the general algorithm 
     clock_t t1_gen = clock(); 
 
-    for (int i = 1; i <= 4; i++) {
+    for (int i = 1; i <= 6; i++) {
         int n = pow(10, i);
         V_special(n, "prob10_gen_n_" + to_string(n) + ".txt");
     }
@@ -123,7 +124,7 @@ int main () {
     // Begin measuring time for the special algorithm 
     clock_t t1_special = clock();
 
-    for (int i = 1; i <= 4; i++) {
+    for (int i = 1; i <= 6; i++) {
         int n = pow(10, i);
         V_special(n, "prob10_spess_n_" + to_string(n) + ".txt");
     }
@@ -134,8 +135,8 @@ int main () {
     double duration_seconds_gen = ((double) (t2_gen - t1_gen)) / CLOCKS_PER_SEC;
     double duration_seconds_special = ((double) (t2_special - t1_special)) / CLOCKS_PER_SEC; 
 
-    //cout << "General Algorithm: " << duration_seconds_gen << " s" << endl; 
-    //cout << "Special Algorithm: " << duration_seconds_special << " s" << endl; 
+    cout << "General Algorithm: " << duration_seconds_gen << " s" << endl; 
+    cout << "Special Algorithm: " << duration_seconds_special << " s" << endl; 
 
     return 0; 
 }
--- Problem9.cpp
+++ Problem9.cpp
@@ -12,45 +12,44 @@ using namespace std;
 using namespace arma;
 
 void V_special(int n, string filename){
-      double h = 1./(n-1);
-      double hh = h*h;
-      vec g(n), v(n);
+    double h = 1./(n-1);
+    double hh = h*h;
+    vec a(n), b(n), c(n), g(n), v(n);
 
-    // all vectors are same number 
-      int a, c = -1;
-      int b = 3/2;
+    // fill vectors a, b, c with values
+    a.fill(-1);
+    b.fill(2);
+    c.fill(-1);
 
-      vec x = linspace(0, 1, n);
-      vec f_x = 100.0*exp(-10*x);
+    vec x = linspace(0, 1, n);
+    vec f_x = 100.0*exp(-10*x);
 
-      g(0) = hh*f_x(0);
-      g = hh*f_x;
-
-      //b = 3/2; 
-      float CONST = -1/2; 
-
-      for (int i = 1; i <= n-2; i++) {
-          g(i) = g(i) - CONST * g(i-1);    
-      }
-
-
-    v(0) = 0;
-    v(n-1) = 0;
-    v(n-2) = g(n-2)/b;
-
-    for (int i = n-3; i >= 1; i--) {
-        v(i) = g(i) / b - CONST*v(i+1);
-
+    g(0) = hh*f_x(0);
+    g = hh*f_x;
+    
+    // calculate b outside the algorithm 
+    for (int i = 1; i < n; i++) {
+        b(i) = (i + 1.)/i;
+    }
+    
+    for (int i = 1; i <= n-2; i++) {
+        g(i) = g(i) - a(i)/b(i-1) * g(i-1);    
     }
 
-    // write x and U(x) to a textfile
+    v(0) = v(n-1) = 0;
+    v(n-2) = g(n-2)/b(n-2);
+
+    for (int i = n-3; i > 0; i--) {
+        v(i) = (g(i) - c(i)*v(i+1)) / b(i);
+    }
+
+    // write x and v to a textfile
     ofstream file;
     file.open(filename, ios::out); //opens file in out/write mode
 
     for (int i = 0; i < n; i++){
         file << setw(25) << setprecision(3) << x(i);
         file <<fixed<< setw(25) << setprecision(8) << v(i) << endl;
-
     }
 
     file.close();
@@ -58,7 +57,7 @@ void V_special(int n, string filename){
 }
 
 int main(int argc, char const *argv[]){ 
-    for (int i = 1; i <= 4; i++) {
+    for (int i = 1; i <= 1; i++) {
         int n = pow(10, i);
         V_special(n, "prob9_n_" + to_string(n) + ".txt");
     }
