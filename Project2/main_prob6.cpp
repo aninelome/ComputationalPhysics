63f636c307eabec43a8f1f34e4e5e2b22cf09528
Anine Lome <aninelo@uio.no>
2021-09-23 12:23:19 +0200

    Made new main files for problem 6 and 7

---
+++ Project2/main_prob6.cpp
@@ -0,0 +1,10 @@
+// Main program for problem 6
+
+#include "functions.hpp"
+
+
+int main(){
+
+    
+   return 0;
+}
---
+++ Project2/main_prob7.cpp
@@ -0,0 +1,10 @@
+// Main program for problem 7
+
+#include "functions.hpp"
+
+
+int main(){
+
+    
+   return 0;
+}
