25a54ffc2dad9bb80f40421e5353da891cdf6e8f
noemiefritz <noemie.fritz@lyse.net>
2021-09-23 13:11:58 +0200

    Worked on problem 6

--- Project2/main_prob6.cpp
+++ Project2/main_prob6.cpp
@@ -3,26 +3,28 @@
 #include "functions.hpp"
 
 int main(){
-  int N = [10, 50, 100];
-  double n = N+1;
-  double h = 1/n;
-  double a = -1/(h*h);
-  double d = 2/(h*h);
-  mat A = make_tridiagonal(N, a, d);
-  mat R = make_tridiagonal(N, 0, 1);
-  int k;
-  int l;
-  double maxA6 = max_offdiag_symmetric(A, &k, &l);
-  vec eigenvalues(N);
-  mat eigenvectors(N,N);
-  int maxiter, iterations;
-  maxiter = 1000000;
-  bool converged;
-  double tol = 1e-10;
-  jacobi_eigensolver(A, R, tol, eigenvalues, eigenvectors, maxiter, 0, converged, k,l);
-  cout << "Eigenvalues" << endl;
-  eigenvalues.print();
-  cout << "Eigenvectors" << endl;
-  eigenvectors.print();
+  vec N = {10, 50, 100, 1000, 10000};
+  for (int i = 0; sizeof(N); i++){
+    double n = N(i)+1;
+    double h = 1/n;
+    double a = -1/(h*h);
+    double d = 2/(h*h);
+    mat A = make_tridiagonal(N(i), a, d);
+    mat R = make_tridiagonal(N(i), 0, 1);
+    int k;
+    int l;
+    double maxA6 = max_offdiag_symmetric(A, &k, &l);
+    vec eigenvalues(N(i));
+    mat eigenvectors(N(i),N(i));
+    int maxiter, iterations;
+    maxiter = 1000000;
+    bool converged;
+    double tol = 1e-10;
+    jacobi_eigensolver(A, R, tol, eigenvalues, eigenvectors, maxiter, 0, converged, k,l);
+    cout << "Eigenvalues" << endl;
+    eigenvalues.print();
+    cout << "Eigenvectors" << endl;
+    eigenvectors.print();
+  }
   return 0;
 }
