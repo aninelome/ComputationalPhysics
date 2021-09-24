a9f6017e826c37861175a3504399e6aaa8a513ff
noemiefritz <noemie.fritz@lyse.net>
2021-09-24 12:33:28 +0200

    made a function for plotting in the common plotting python file

--- Project2/pythonplot_prob6.py
+++ Project2/pythonplot_prob6.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-N = np.loadtxt("plot_prob6.txt", usecols=0)
-it = np.loadtxt("plot_prob6.txt", usecols=1)
-plt.title("Number of transformations versus dimension of matrix")
-plt.xlabel("N")
-plt.ylabel("Number of iterations")
-plt.loglog(N, it)
-plt.loglog(N, it, "ro")
-plt.show()
-
+def plot_prob6(filename):
+    N = np.loadtxt("plot_prob6.txt", usecols=0)
+    it = np.loadtxt("plot_prob6.txt", usecols=1)
+    plt.title("Number of transformations versus dimension of matrix")
+    plt.xlabel("N")
+    plt.ylabel("Number of iterations")
+    plt.loglog(N, it)
+    plt.loglog(N, it, "ro")
+    plt.show()
+return
 #Finding the slope of the graph to find the scale between number of transformations and N
 s = (np.log10(it[-1]) - np.log10(it[0]))/(np.log10(N[-1]) - np.log10(N[0]))
 print(f"Scale = {s}")
