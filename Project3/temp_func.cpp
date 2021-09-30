9763a238c455c6cca7c2a05bb9fc585bcb5dc53f
semyaat <semya@erlito.com>
2021-09-30 13:47:52 +0200

    changes to temp_func.cpp

--- Project3/temp_func.cpp
+++ Project3/temp_func.cpp
@@ -9,30 +9,36 @@ using namespace arma;
 
 // col er et tidssteg 
 
-void forward_euler(int n = 10);
+
 
 /// denne må føres som en algoritme 
 void forward_euler(int n) {
     int h = 1; 
     int m = 1; 
-    // definer matrise til å være 3*n 
+    // Define the matrice as 3*n 
     mat v, r; 
     v = mat(3,n).fill(0); 
     r = mat(3,n).fill(0); 
     vec t = vec(n).fill(0); 
 
+    int F = 10; 
+
+
+    // må sjekke n - 1 
+    for (int i = 0; i = n-2; i++) {
+        for (int j = 0; j = 2; j++) {
+        int a = F/m;                  // m hentes fra klassen 
+        v(j,i+1) = v(j,i) + a*h;
+        r(j,i+1) = r(j,i) + v(j,i) * h; 
+        t(i+1) = t(i) + h; 
+        }
+    }
     cout << v << endl; 
 
-    
-    /* 
-    for (int i = 0; i = n-1; i++) {
-        mat a = F/m;                  // m hentes fra klassen 
-        v(:,i+1) = v(:,i) + a*h;
-        r(:,i+1) = r(:,i) + v(:,i) * h; 
-        t(i+1) = t(i) + h; 
-    }
-    cout << v << r << t << endl; 
-    */ 
 
+}
 
+int main() {
+    forward_euler(3);
+    return 0; 
 }
