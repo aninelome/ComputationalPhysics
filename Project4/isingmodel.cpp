a2756b771ac69e83f0fa0da50fb73f48ad235669
noemiefritz <noemie.fritz@lyse.net>
2021-11-11 14:31:20 +0100

    merge problem

--- Project4/isingmodel.cpp
+++ Project4/isingmodel.cpp
@@ -144,7 +144,7 @@ void IsingModel::mcmc(vec* eps_vec, vec* m_abs_vec)
     cout << "M_abs = " << M_abs << endl;
     cout << "-----------------------------------\n"
          << endl;
-         
+
     double eps_exp = E_tot / (N_cycles_ * N);
     double m_exp = M_tot / (N_cycles_ * N);
     double eps2_exp = (E_tot2 / N_cycles_) *1/(N * N);
@@ -161,35 +161,24 @@ void IsingModel::mcmc(vec* eps_vec, vec* m_abs_vec)
     cout << "m2_exp = " << m2_exp << endl;
 }
 
-<<<<<<< HEAD
 void IsingModel::metropolis_burnin(imat &S)
 {
-   
+
     int i_index = randi(distr_param(0,L_-1));
     int j_index = randi(distr_param(0,L_-1));
 
     int dE = delta_E(S, i_index, j_index);
-=======
-// void IsingModel::metropolis(imat &S, double *E_sys, double *M_sys)
-// {
 
-//     int i_index = randi(distr_param(0,L_-1));
-//     int j_index = randi(distr_param(0,L_-1));
->>>>>>> edbbf509d801c423effc89733adedb014a1dab13
-
-
-<<<<<<< HEAD
     if (dE <= 0)
     {
         S(i_index, j_index) *= -1;
-      
+
     }
     else if(randu() <= boltzmann_factor(boltzmann_list, dE)){
         S(i_index, j_index) *= -1;
 
     }
 }
-
 
 void IsingModel::burnintime(int N_burn)
 {
@@ -204,85 +193,6 @@ void IsingModel::burnintime(int N_burn)
         for (int j = 0; j < N; j++)
         {
             metropolis_burnin(S);
-        }  
+        }
     }
 }
-=======
-//     if (dE <= 0)
-//     {
-//         S(i_index, j_index) *= -1;
-
-//     }
-//     else if(randu() <= boltzmann_factor(boltzmann_list, dE)){
-//         S(i_index, j_index) *= -1;
-
-//     }
-// }
-
-
-// void IsingModel::burnintime(int N_burn)
-// {
-//     double E_sys;
-//     double E_tot;
-//     double E_tot2;
-//     double M_sys = 0;
-//     double M_tot;
-//     double M_tot2;
-//     double M_abs;
-//     int N = L_ * L_;
-//     boltzmann_list = {exp(8 * beta_), 0, 0, 0, exp(4 * beta_), 0, 0, 0, 1, 0, 0, 0, exp(-4 * beta_), 0, 0, 0, exp(-8 * beta_)};
-
-//     imat S = make_matrix(&M_sys);
-
-//     E_sys = 1.*energy(S);
-//     E_tot = E_sys;
-//     E_tot2 = E_sys * E_sys;
-//     M_tot = M_sys;
-//     M_tot2 = M_sys * M_sys;
-//     (*eps_vec)(0) = E_tot/N_cycles_;
-//     (*m_abs_vec)(0) = M_abs/N_cycles_;
-
-//     cout << "E0 = " << E_sys << endl;
-//     cout << "M0 = " << M_sys << endl;
-
-//     for (int i = 0; i < N_cycles_; i++)
-//     {
-//         for (int j = 0; j < N; j++)
-//         {
-//             metropolis(S, &E_sys, &M_sys);
-//         }
-//     }
-// }
->>>>>>> edbbf509d801c423effc89733adedb014a1dab13
-
-/*
-double M(int L) {
-    // Func. that returns the total magnetization of the system
-    // given by the sum over all the spins in the system
-    double sum = 0;
-    int N = L**2;
-    for (i=0; i=N-1; i++) {
-        sum += S(i);
-    }
-    return sum
-}
-
-
-double eps(mat S, double J, int L, int N) {
-    // function that returns energy per spin
-    eps = E(mat S, double J, int L)/N;
-    mean_eps = integrate eps(s) * probability(s) ds;
-    return eps, mean_eps;
-}
-
-double m(int L) {
-    // function that returns magnetization per spin
-    m = M(L)/N;
-    mean_m = ;
-    return m, mean_m;
-}
-
-double Cv() {
-    return 1/(kB*T**2) * ()
-}
-*/
